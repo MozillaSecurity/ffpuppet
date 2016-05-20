64b04bb0755acd50576c3b868449dd5cd3e80efc
Tyson Smith <tyson.w.smith@gmail.com>
2016-05-20 10:20:33 -0700

    Don't generate prefs.js

--- ffpuppet.py
+++ ffpuppet.py
@@ -70,111 +70,6 @@ class FFPuppet(object):
                 raise LaunchException('Could not launch Xvfb!')
 
 
-    def _create_profile(self):
-        self._profile_dir = tempfile.mkdtemp(prefix='ffprof_')
-        self.build_prefs(self._profile_dir)
-
-
-    @staticmethod
-    def build_prefs(file_dir):
-        prefs = {
-            'app.update.enabled':False,
-            'app.update.staging.enabled':False,
-            'browser.cache.disk.enable':False,
-            'browser.cache.disk_cache_ssl':False,
-            'browser.cache.memory.enable':False,
-            'browser.cache.offline.enable':False,
-            'browser.chrome.favicons':False,
-            'browser.chrome.site_icons':False,
-            'browser.displayedE10SNotice':2,
-            'browser.dom.window.dump.enabled':True, # Prints messages to the (native) console
-            'browser.EULA.override':True,
-            'browser.firstrun.show.localepicker':False,
-            'browser.firstrun.show.uidiscovery':False,
-            'browser.microsummary.enabled':False,
-            'browser.offline-apps.notify':False,
-            'browser.rights.3.shown':True,
-            'browser.safebrowsing.enabled':False,
-            'browser.safebrowsing.malware.enabled':False,
-            'browser.search.update':False,
-            'browser.sessionhistory.max_total_viewers':1,
-            'browser.sessionstore.resume_from_crash':False,
-            'browser.shell.checkDefaultBrowser':False,
-            'browser.startup.homepage':'"about:blank"',
-            'browser.startup.homepage_override.mstone':'"ignore"',
-            'browser.startup.page':0, # use about:blank
-            'browser.tabs.remote.autostart':False, # disable e10s
-            'browser.tabs.remote.autostart.1':False, # disable e10s
-            'browser.tabs.remote.autostart.2':False, # disable e10s
-            'browser.tabs.warnOnClose':False,
-            'browser.tabs.warnOnCloseOtherTabs':False,
-            'browser.webapps.checkForUpdates':0,
-            'datareporting.policy.dataSubmissionEnabled':False,
-            'datareporting.policy.dataSubmissionPolicyAcceptedVersion':2,
-            'datareporting.healthreport.service.enabled':False,
-            'datareporting.healthreport.service.firstRun':False,
-            'datareporting.healthreport.uploadEnabled':False,
-            'dom.allow_scripts_to_close_windows':True,
-            'dom.disable_open_during_load':False, # Determines popup blocker behavior
-            'dom.disable_window_flip':False, # Determines whether windows can be focus()ed via non-chrome JavaScript
-            'dom.disable_window_move_resize':False,
-            'dom.disable_window_status_change':False, # text in the browser status bar may be set by non-chrome JavaScript
-            'dom.ipc.plugins.flash.subprocess.crashreporter.enabled':False,
-            'dom.max_chrome_script_run_time':0,
-            'dom.max_script_run_time':0,
-            'dom.min_background_timeout_value':4,
-            'dom.send_after_paint_to_content':True, # needed when using IMGCorpman with MozAfterPaint event
-            'extensions.blocklist.enabled':False,
-            'extensions.testpilot.runStudies':False,
-            'extensions.update.enabled':False,
-            'general.warnOnAboutConfig':False,
-            'geo.enabled':False,
-            'image.cache.size':0,
-            'image.multithreaded_decoding.limit':1,
-            'layout.debug.enable_data_xbl':True,
-            'lightweightThemes.update.enabled':False,
-
-            #'media.autoplay.enabled':False,
-            'media.mediasource.enabled':True,
-            'media.mediasource.mp4.enabled':True,
-            'media.fragmented-mp4.enabled':True,
-            'media.fragmented-mp4.exposed':True,
-            'media.fragmented-mp4.ffmpeg.enabled':True,
-            'media.fragmented-mp4.gmp.enabled':True,
-            'media.ogg.enabled':True,
-            'media.opus.enabled':True,
-            #'media.use-blank-decoder':True,
-            'media.wave.decoder.enabled':True,
-            'media.wave.enabled':True,
-
-            'network.http.max-connections':1,
-            'network.http.spdy.enabled':False,
-            'network.http.use-cache':False,
-            'network.prefetch-next':False,
-            #'network.proxy.share_proxy_settings':True,
-            #'network.proxy.type':2,
-            #'network.proxy.autoconfig_url':'"data:text/plain,function FindProxyForURL(url, host) ' \
-            #                               '{ if (host == \'localhost\' || host == \'127.0.0.1\') ' \
-            #                               '{ return \'DIRECT\'; } else { return \'PROXY 127.0.0.1:6\'; } }"',
-            'network.network.protocol-handler.external.mailto':False,
-            'nglayout.debug.disable_xul_cache':False,
-            'plugins.hide_infobar_for_missing_plugin':True,
-            'plugins.update.url':'""',
-            'security.fileuri.strict_origin_policy':False,
-            'security.OCSP.enabled':0,
-            'shumway.disabled':True,
-            'toolkit.startup.max_resumed_crashes':-1,
-            'toolkit.telemetry.prompted':2,
-            'toolkit.telemetry.rejected':True,
-            'toolkit.telemetry.server':'""',
-        }
-
-        file_path = os.path.join(file_dir, 'prefs.js')
-        with open(file_path, 'w') as fp:
-            for key, value in prefs.items():
-                fp.write('user_pref("%s", %s);\n' % (key, str(value).lower()))
-
-
     def close(self, save_log=None):
         if self._proc is not None:
             if self._proc.poll() is None:
@@ -211,7 +106,7 @@ class FFPuppet(object):
         return None if self._proc is None else self._proc.pid
 
 
-    def launch(self, bin_path, launch_timeout=300, location=None, memory_limit=None):
+    def launch(self, bin_path, launch_timeout=300, location=None, memory_limit=None, prefs_js=None):
         if launch_timeout is None or launch_timeout < 1:
             raise LaunchException("Launch timeout must be >= 1")
 
@@ -259,7 +154,9 @@ class FFPuppet(object):
                 ))
 
         if self._profile_dir is None:
-            self._create_profile()
+            self._profile_dir = tempfile.mkdtemp(prefix='ffprof_')
+            if prefs_js:
+                shutil.copyfile(prefs_js, os.path.join(self._profile_dir, 'prefs.js'))
 
         fd, self._log = tempfile.mkstemp(
             suffix='_log.txt',
@@ -473,7 +370,10 @@ if __name__ == '__main__':
         '-m', '--memory', type=int, default=None,
         help='Process memory limit in MBs')
     parser.add_argument(
-        '-p', '--profile', default=None,
+        '-p', '--prefs',
+        help='prefs.js file to use')
+    parser.add_argument(
+        '-P', '--profile', default=None,
         help='profile to use')
     parser.add_argument(
         '-t', '--timeout', type=int, default=60,
@@ -499,7 +399,8 @@ if __name__ == '__main__':
             args.binary,
             location=args.url,
             launch_timeout=args.timeout,
-            memory_limit=args.memory * 1024 * 1024 if args.memory else None)
+            memory_limit=args.memory * 1024 * 1024 if args.memory else None,
+            prefs_js=args.prefs)
         ffp.wait()
     except KeyboardInterrupt:
         print("Ctrl+C detected. Shutting down...")
