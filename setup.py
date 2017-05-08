a2e7b224b917cd3caf45e191e90bfdca137056c4
Tyson Smith <tyson.w.smith@gmail.com>
2017-05-07 22:11:05 -0700

    Fix setup.py, thanks pyoor!

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-import sys
 from setuptools import setup
 
 if __name__ == '__main__':
-    plat = {'linux2': 'linux',
-            'win32': 'windows',
-            'darwin': 'mac'}[sys.platform]
-    with open('requirements-%s.txt' % plat) as f:
+    with open('requirements.txt') as f:
         requires = f.read().strip().splitlines()
-    setup(name = "ffpuppet",
-          version = "0.1",
+    setup(name="ffpuppet",
+          version="0.1",
           install_requires=requires,
-          packages = ['ffpuppet'],
-          package_dir = {'ffpuppet': ''})
+          packages=['ffpuppet'],
+          package_dir={'ffpuppet': ''})
 
