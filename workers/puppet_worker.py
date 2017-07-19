3e2f738d7e966c0e3fc7cd9e81bcfdb28d78ac78
Tyson Smith <tyson.w.smith@gmail.com>
2017-07-19 14:58:51 -0700

    Dump backtrace when the memory limit is hit

--- workers/puppet_worker.py
+++ workers/puppet_worker.py
@@ -3,6 +3,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import re
+import subprocess
 import tempfile
 import time
 
@@ -54,3 +56,48 @@ class BaseWorker(object):
     def join(self):
         if self._worker is not None:
             self._worker.join()
+
+
+def gdb_log_dumpper(pid):
+    try:
+        gdb_bin = subprocess.check_output(["which", "gdb"]).decode("utf-8").strip()
+    except subprocess.CalledProcessError:
+        return "Could not dump process info, gdb is not installed"
+
+    tmp_fd, cmd_file = tempfile.mkstemp(
+        suffix="_gdb.cmd",
+        prefix=time.strftime("ffpworker_%m-%d_%H-%M-%S_"))
+    os.close(tmp_fd)
+    try:
+        # create temporary cmd file
+        with open(cmd_file, "w") as cmd_fp:
+            cmd_fp.write("if $_siginfo\n")
+            cmd_fp.write("  echo connected\n")
+            cmd_fp.write("else\n")
+            cmd_fp.write("  quit 1\n")
+            cmd_fp.write("end\n")
+            cmd_fp.write("echo \\n received signal SIG (this is here to trigger FM parsing)\\n\n")
+            cmd_fp.write("bt\n")
+            cmd_fp.write("info threads\n")
+            cmd_fp.write("quit\n")
+
+        # this requires kernel.yama.ptrace_scope=0
+        # on Ubuntu run "sudo sysctl kernel.yama.ptrace_scope=0"
+        gdb_cmd = [gdb_bin, "-q", "-batch", "-x", cmd_file, "-p", "%d" % pid]
+
+        with tempfile.TemporaryFile(mode="w+") as log_fp:
+            try:
+                subprocess.check_call(gdb_cmd, shell=False, stderr=log_fp, stdout=log_fp)
+            except subprocess.CalledProcessError:
+                log_fp.seek(0)
+                return "Error executing: %s\n%s" % (" ".join(gdb_cmd), log_fp.read())
+            log_fp.seek(0)
+
+            flt = re.compile(
+                r"(\[New LWP \d+\]|" \
+                r"No locals\.|" \
+                r"Quit anyway\? \(y or n\) \[answered Y; input not from terminal\])$")
+            return "\n".join([x for x in log_fp.read().splitlines() if flt.match(x) is None])
+    finally:
+        if os.path.isfile(cmd_file):
+            os.remove(cmd_file)
