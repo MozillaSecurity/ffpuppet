937dae1e2cc5aade3787697bf9e48f4d32a45085
Tyson Smith <tyson.w.smith@gmail.com>
2018-10-10 14:46:14 -0700

    [tests] Add SIGINT test

--- ffpuppet/test_main.py
+++ ffpuppet/test_main.py
@@ -5,11 +5,13 @@
 import logging
 import os
 import shutil
+import signal
+import subprocess
 import sys
 import tempfile
 import unittest
 
-from ffpuppet import FFPuppet
+import ffpuppet
 from .main import main
 
 logging.basicConfig(level=logging.DEBUG if bool(os.getenv("DEBUG")) else logging.INFO)
@@ -19,8 +21,8 @@ CWD = os.path.realpath(os.path.dirname(__file__))
 TESTFF_BIN = os.path.join(CWD, "testff", "testff.exe") if sys.platform.startswith('win') else os.path.join(CWD, "testff.py")
 TESTMDSW_BIN = os.path.join(CWD, "testmdsw", "testmdsw.exe") if sys.platform.startswith('win') else os.path.join(CWD, "testmdsw.py")
 
-FFPuppet.MDSW_BIN = TESTMDSW_BIN
-FFPuppet.MDSW_MAX_STACK = 8
+ffpuppet.FFPuppet.MDSW_BIN = TESTMDSW_BIN
+ffpuppet.FFPuppet.MDSW_MAX_STACK = 8
 
 class TestCase(unittest.TestCase):
 
@@ -62,3 +64,26 @@ class MainTests(TestCase):
         with open(prefs, "w") as prefs_fp:
             prefs_fp.write("//fftest_big_log\n")
         main([TESTFF_BIN, "-v", "-d", "-p", prefs, "--log-limit", "1", "-a", "blah_test"])
+
+    def test_04(self):
+        "test sending SIGINT"
+        out_logs = os.path.join(self.tmpdir, "logs")
+        with tempfile.TemporaryFile() as console:
+            proc = subprocess.Popen(
+                [sys.executable, "-m", "ffpuppet", TESTFF_BIN, "-l", out_logs],
+                cwd=os.path.split(os.path.split(ffpuppet.__file__)[0])[0],
+                shell=False,
+                stderr=console,
+                stdout=console)
+            self.assertIsNone(proc.poll())
+            while proc.poll() is None:
+                console.seek(0)
+                if b"launched" in console.read():
+                    break
+            os.kill(proc.pid, signal.SIGINT)
+            self.assertIsNotNone(proc.wait())
+            console.seek(0)
+            output = console.read()
+        self.assertIn(b"Ctrl+C detected", output)
+        self.assertTrue(os.path.isdir(out_logs))
+        self.assertGreater(len(os.listdir(out_logs)), 0)
