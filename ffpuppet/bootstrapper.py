adb471341801b2f7164749a9f5bda48a39bbf4b2
Tyson Smith <tyson.w.smith@gmail.com>
2020-09-29 09:19:56 -0700

    Bootstrapper nits and cleanup

--- ffpuppet/bootstrapper.py
+++ ffpuppet/bootstrapper.py
@@ -1,17 +1,17 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import errno
-import logging
-import platform
-import random
+from errno import EADDRINUSE
+from logging import getLogger
+from platform import system
+from random import randint
 import socket
-import time
+from time import time
 
 from .exceptions import BrowserTerminatedError, BrowserTimeoutError, LaunchError
 
 
-log = logging.getLogger("ffpuppet")  # pylint: disable=invalid-name
+log = getLogger("ffpuppet")  # pylint: disable=invalid-name
 
 __author__ = "Tyson Smith"
 __all__ = ("Bootstrapper",)
@@ -19,29 +19,30 @@ __all__ = ("Bootstrapper",)
 
 class Bootstrapper(object):
     BUF_SIZE = 4096  # receive buffer size
+    POLL_WAIT = 1  # duration of initial blocking socket operations
     PORT_MAX = 0xFFFF  # bootstrap range
     PORT_MIN = 0x2000  # bootstrap range
     PORT_RETRIES = 100  # number of attempts to find an available port
 
     __slots__ = ("_socket",)
 
-    def __init__(self, poll_wait=0.25):
+    def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if platform.system().startswith("Windows"):
+        if system().startswith("Windows"):
             self._socket.setsockopt(
                 socket.SOL_SOCKET,
                 socket.SO_EXCLUSIVEADDRUSE,  # pylint: disable=no-member
                 1)
-        self._socket.settimeout(poll_wait)
+        self._socket.settimeout(self.POLL_WAIT)
         for _ in range(self.PORT_RETRIES):
             try:
-                self._socket.bind(("127.0.0.1", random.randint(self.PORT_MIN, self.PORT_MAX)))
+                self._socket.bind(("127.0.0.1", randint(self.PORT_MIN, self.PORT_MAX)))
                 self._socket.listen(5)
             except socket.error as soc_e:
-                if soc_e.errno in (errno.EADDRINUSE, 10013):
+                if soc_e.errno in (EADDRINUSE, 10013):
                     # Address already in use
                     continue
-                raise soc_e  # pragma: no cover
+                raise  # pragma: no cover
             break
         else:
             self._socket.close()
@@ -70,21 +71,22 @@ class Bootstrapper(object):
 
     def wait(self, cb_continue, timeout=60, url=None):
         assert self._socket is not None
-        start_time = time.time()
+        start_time = time()
         time_limit = start_time + timeout
+        conn = None
         try:
-            conn = None
             log.debug("waiting for browser connection...")
             while True:
                 try:
                     conn, _ = self._socket.accept()
                 except socket.timeout:
-                    if not cb_continue():
-                        raise BrowserTerminatedError("Failure waiting for browser connection")
-                    if time.time() >= time_limit:
-                        raise BrowserTimeoutError("Timeout waiting for browser connection")
-                    continue
-                break
+                    conn = None
+                if conn is not None:
+                    break
+                if not cb_continue():
+                    raise BrowserTerminatedError("Failure waiting for browser connection")
+                if time() >= time_limit:
+                    raise BrowserTimeoutError("Timeout waiting for browser connection")
             conn.settimeout(1)
             received = False
             log.debug("waiting to receive browser request...")
@@ -94,7 +96,7 @@ class Bootstrapper(object):
                 except socket.timeout:
                     if not cb_continue():
                         raise BrowserTerminatedError("Failure waiting for request")
-                    if time.time() >= time_limit:
+                    if time() >= time_limit:
                         raise BrowserTimeoutError("Timeout waiting for request")
                     if not received:
                         continue
@@ -112,7 +114,7 @@ class Bootstrapper(object):
                 resp = "HTTP/1.1 301 Moved Permanently\r\n" \
                        "Location: %s\r\n" \
                        "Connection: close\r\n\r\n" % url
-            conn.settimeout(max(int(time_limit - time.time()), 1))
+            conn.settimeout(max(int(time_limit - time()), 1))
             log.debug("sending response (redirect %r)", url)
             try:
                 conn.sendall(resp.encode("ascii"))
@@ -124,7 +126,7 @@ class Bootstrapper(object):
                 raise BrowserTerminatedError("Failure during browser startup")
             if resp_timeout:
                 raise BrowserTimeoutError("Timeout sending response")
-            log.debug("bootstrap complete (%0.2fs)", time.time() - start_time)
+            log.debug("bootstrap complete (%0.2fs)", time() - start_time)
         except socket.error as soc_e:  # pragma: no cover
             raise LaunchError("Error attempting to launch browser: %s" % soc_e)
         finally:
