1b3bd9ac25613d9e74940aa591c1fa9d81b293e7
Tyson Smith <tyson.w.smith@gmail.com>
2021-06-15 10:10:01 -0700

    Update top level __all__

--- ffpuppet/__init__.py
+++ ffpuppet/__init__.py
@@ -4,8 +4,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from .core import FFPuppet
+from .core import Debugger, FFPuppet, Reason
 from .exceptions import BrowserTerminatedError, BrowserTimeoutError, LaunchError
 
-__all__ = ("FFPuppet", "BrowserTimeoutError", "BrowserTerminatedError", "LaunchError")
+__all__ = (
+    "Debugger",
+    "FFPuppet",
+    "Reason",
+    "BrowserTimeoutError",
+    "BrowserTerminatedError",
+    "LaunchError",
+)
 __author__ = "Tyson Smith"
