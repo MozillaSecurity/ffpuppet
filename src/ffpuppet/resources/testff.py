e71f7fd131a5e19f38c6d47fb2768c7c4164a307
Tyson Smith <tyson.w.smith@gmail.com>
2024-09-10 16:49:13 -0700

    [tests] Update browser mocking scripts

--- src/ffpuppet/resources/testff.py
+++ src/ffpuppet/resources/testff.py
@@ -1,101 +1,103 @@
 #!/usr/bin/env python
 """fake firefox"""
 
-import os
-import platform
 import sys
-import time
+from argparse import ArgumentParser
+from enum import IntEnum, auto, unique
+from pathlib import Path
+from time import sleep
 from urllib.error import URLError
 from urllib.request import urlopen
 
 EXIT_DELAY = 45
 
 
-def main() -> int:  # pylint: disable=missing-docstring
-    os_name = platform.system()
-    profile = url = None
-    while len(sys.argv) > 1:
-        arg = sys.argv.pop(1)
-        if arg in ("-headless", "-new-instance"):
-            pass
-        elif os_name == "Windows" and arg in ("-no-deelevate", "-wait-for-browser"):
-            pass
-        elif arg.startswith("http://"):
-            url = arg
-        elif arg == "-profile":
-            profile = sys.argv.pop(1)
-        else:
-            raise RuntimeError(f"unknown argument: {arg}")
-    if url is None:
-        sys.stderr.write("missing url\n")
-        return 1
+@unique
+class Mode(IntEnum):
+    """Available testing modes"""
+
+    BIG_LOG = auto()
+    EXIT_CODE = auto()
+    INVALID_JS = auto()
+    MEMORY = auto()
+    NONE = auto()
+    SOFT_ASSERT = auto()
+
+
+def main() -> int:
+    """Fake Firefox for testing"""
+    parser = ArgumentParser(prog="testff", description="Fake Firefox for testing")
+    parser.add_argument("url")
+    parser.add_argument("-headless", action="store_true", help="ignored")
+    parser.add_argument("-new-instance", action="store_true", help="ignored")
+    parser.add_argument("-no-deelevate", action="store_true", help="ignored")
+    parser.add_argument("-wait-for-browser", action="store_true", help="ignored")
+    parser.add_argument("-profile", type=Path, required=True)
+    args = parser.parse_args()
+
     # read prefs to see how to run
-    cmd = None
     exit_code = 0
-    if profile is not None:
-        with open(os.path.join(profile, "prefs.js")) as prefs_js:
-            for line in prefs_js:
-                if line.startswith("user_pref"):
-                    pass
-                elif line.startswith("/"):
-                    line = line.lstrip("/").strip()
-                    if line == "fftest_memory":
-                        cmd = "memory"
-                    elif line == "fftest_soft_assert":
-                        cmd = "soft_assert"
-                    elif line == "fftest_invalid_js":
-                        cmd = "invalid_js"
-                    elif line == "fftest_big_log":
-                        cmd = "big_log"
-                    elif line.startswith("fftest_exit_code_"):
-                        cmd = "exit_code"
-                        exit_code = int(line.split("fftest_exit_code_")[-1])
-                    # don't worry about unknown values
-                elif line.startswith("#"):
-                    pass  # skip comments
-                elif line.strip():
-                    raise RuntimeError(f"unknown value in prefs.js: {line}")
+    mode = Mode.NONE
+    with (args.profile / "prefs.js").open() as prefs_js:
+        for line in prefs_js:
+            if line.startswith("user_pref"):
+                pass
+            elif line.startswith("/"):
+                line = line.lstrip("/").strip()
+                if line == "fftest_memory":
+                    mode = Mode.MEMORY
+                elif line == "fftest_soft_assert":
+                    mode = Mode.SOFT_ASSERT
+                elif line == "fftest_invalid_js":
+                    mode = Mode.INVALID_JS
+                elif line == "fftest_big_log":
+                    mode = Mode.BIG_LOG
+                elif line.startswith("fftest_exit_code_"):
+                    mode = Mode.EXIT_CODE
+                    exit_code = int(line.split("fftest_exit_code_")[-1])
+                # don't worry about unknown values
+            elif line.startswith("#"):
+                pass  # skip comments
+            elif line.strip():
+                raise RuntimeError(f"unknown value in prefs.js: {line}")
     # sys.stdout.write(f'cmd: {cmd}\n')
     # sys.stdout.flush()
 
-    assert profile is not None
-    if cmd == "invalid_js":
-        with open(os.path.join(profile, "Invalidprefs.js"), "w") as prefs_js:
-            prefs_js.write("bad!")
+    if mode == Mode.INVALID_JS:
+        (args.profile / "Invalidprefs.js").write_text("bad!")
 
     target_url = None
-    if url:
-        try:
-            # pylint: disable=consider-using-with
-            conn = urlopen(url)
-        except URLError as req_err:
-            # can't redirect to file:// from http://
-            # pylint: disable=consider-using-with
-            conn = urlopen(str(req_err.reason).split("'")[1])
-        try:
-            target_url = conn.geturl()
-            if target_url == url:
-                target_url = None
-            sys.stdout.write(conn.read().decode())
-            sys.stdout.write("\n")
-            sys.stdout.flush()
-        finally:
-            conn.close()
+    try:
+        # pylint: disable=consider-using-with
+        conn = urlopen(args.url)
+    except URLError as req_err:
+        # can't redirect to file:// from http://
+        # pylint: disable=consider-using-with
+        conn = urlopen(str(req_err.reason).split("'")[1])
+    try:
+        target_url = conn.geturl()
+        if target_url == args.url:
+            target_url = None
+        sys.stdout.write(conn.read().decode())
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+    finally:
+        conn.close()
 
     sys.stdout.write(f"url: {target_url!r}\n")
     sys.stdout.flush()
 
-    if cmd == "memory":
+    if mode == Mode.MEMORY:
         sys.stdout.write("simulating high memory usage\n")
         sys.stdout.flush()
         _ = ["A" * 1024 * 1024 for _ in range(200)]
-    elif cmd == "soft_assert":
+    elif mode == Mode.SOFT_ASSERT:
         sys.stdout.write("simulating soft assertion\n")
         sys.stdout.flush()
         sys.stderr.write("A" * 512 * 1024)
         sys.stderr.write("\n###!!! ASSERTION: test\n\nblah...\n")
         sys.stderr.flush()
-    elif cmd == "big_log":
+    elif mode == Mode.BIG_LOG:
         sys.stdout.write("simulating big logs\n")
         buf = "A" * (512 * 1024)  # 512KB
         for _ in range(25):
@@ -103,14 +105,14 @@ def main() -> int:  # pylint: disable=missing-docstring
             sys.stderr.write(buf)
             sys.stdout.flush()
             sys.stderr.flush()
-    elif cmd == "exit_code":
+    elif mode == Mode.EXIT_CODE:
         sys.stdout.write(f"exit code test ({exit_code})\n")
         sys.stdout.flush()
         return exit_code
 
     sys.stdout.write(f"running... (sleep {EXIT_DELAY})\n")
     sys.stdout.flush()
-    time.sleep(EXIT_DELAY)  # wait before closing (should be terminated before elapse)
+    sleep(EXIT_DELAY)  # wait before closing (should be terminated before elapse)
     sys.stdout.write("exiting normally\n")
     sys.stdout.flush()
     return 0
