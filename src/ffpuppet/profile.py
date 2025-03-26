0b0e611355dc2ff95d6ea10ca365b12cd0366df0
Tyson Smith <tyson.w.smith@gmail.com>
2025-03-25 17:04:18 -0700

    ci: replace black with ruff-format

--- src/ffpuppet/profile.py
+++ src/ffpuppet/profile.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """ffpuppet profile manager"""
+
 from __future__ import annotations
 
 from json import load as json_load
--- src/ffpuppet/test_display.py
+++ src/ffpuppet/test_display.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 """display.py tests"""
+
 from platform import system
 
 from pytest import mark, raises
